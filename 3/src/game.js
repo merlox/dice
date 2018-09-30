0deeffe4927bc9e68da8af775e1e11dbe7249db9
merlox <merloxdixcontrol@gmail.com>
2018-09-30 22:11:02 +0200

    Working setup of bets

--- 3/src/game.js
+++ 3/src/game.js
@@ -1,6 +1,8 @@
 let activeDice = 0
 let game
 let socket
+let isThisPlayer1 = false
+let isThisPlayer2 = false
 
 start()
 
@@ -10,10 +12,12 @@ function start() {
     setListeners()
 
     let game = JSON.parse(localStorage.getItem('game'))
+    isThisPlayer1 = game.isPlayer1
+    isThisPlayer2 = game.isPlayer2
     socket.emit('get-players-data', game.contractAddress)
 }
 
-function setDiceListeners() {
+function setListeners() {
     document.querySelectorAll('.dice-image').forEach(dice => {
         dice.addEventListener('click', e => {
             // Set the active dice data
@@ -31,9 +35,16 @@ function setDiceListeners() {
     })
 
     document.querySelector('.place-bet').addEventListener('click', () => {
-        if(activeDice == 0) return status('You must select a dice before placing the bet')
+        let bet = document.querySelector('.bet-input').value
 
-        document.querySelector('.bet-input').value
+        if(activeDice == 0) return status('You must select a dice before placing the bet')
+        if(bet == 0) return status('You must place a bet larger than zero')
+        if(isThisPlayer1 && bet > game.balancePlayer1) return status("You can't bet higher than your current balance of " + web3.fromWei(game.balancePlayer1) + ' ether')
+        if(isThisPlayer2 && bet > game.balancePlayer2) return status("You can't bet higher than your current balance of " + web3.fromWei(game.balancePlayer2) + ' ether')
+        if(isThisPlayer1 && bet > game.escrowPlayer1) return status("You can't bet higher than your escrow of " + web3.fromWei(game.balancePlayer1) + ' ether')
+        if(isThisPlayer2 && bet > game.escrowPlayer2) return status("You can't bet higher than your escrow of " + web3.fromWei(game.balancePlayer2) + ' ether')
+
+        placeBet(bet)
     })
 
     socket.on('game-data', gameData => {
@@ -41,21 +52,12 @@ function setDiceListeners() {
     })
 }
 
-function placeBet(amount, number) {
-
-}
+// This function takes care of generating the messages with the 'activeDice' and the bet used
+function placeBet(bet) {}
 
 function status(message) {
     document.querySelector('.status').innerHTML = message
     setTimeout(() => {
         document.querySelector('.status').innerHTML = ''
     }, 3e3)
-}
-
-function callPlayer1() {
-
-}
-
-function callPlayer2() {
-
 }
